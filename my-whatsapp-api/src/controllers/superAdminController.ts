b108a5931f6ddd3c98edf98dd25da3c35c88b127
Parth <parthuvichare9@gmail.com>
2025-02-11 17:05:56 +0530

    Commit

--- my-whatsapp-api/src/controllers/superAdminController.ts
+++ my-whatsapp-api/src/controllers/superAdminController.ts
@@ -3,39 +3,39 @@
 
 
 
-// // Conversation & Chat History
-// router.get("/chat")
-// router.get("/chat/:lead_id")
+// Conversation & Chat History
+router.get("/chat")
+router.get("/chat/:lead_id")
 
-// //Live-Chat
-// router.get("/live")
-// router.get("/live/:lead_id")
+//Live-Chat
+router.get("/live")
+router.get("/live/:lead_id")
 
-// //Lead Management
-// router.get("/contacts")
-// router.post("/assignLead")
-// router.get("/lead/:leadId")
+//Lead Management
+router.get("/contacts")
+router.post("/assignLead")
+router.get("/lead/:leadId")
 
-// // Sales Agent Monitoring
-// router.get("/salesAgents")
-// router.get("/salesAgents/:agentId")
-// router.get("/salesAgents/active")
-
-
-// //Message Controlling
-// router.post("/sendMessage")
-// router.post("/broadcast")
-// router.post("/triggerNotification")
+// Sales Agent Monitoring
+router.get("/salesAgents")
+router.get("/salesAgents/:agentId")
+router.get("/salesAgents/active")
 
 
-// //Template Management
-// router.get("/templates")
-// router.get("/templates/:templateId")
-// router.post("/editTemplate/:templateId")
+//Message Controlling
+router.post("/sendMessage")
+router.post("/broadcast")
+router.post("/triggerNotification")
 
 
-// // Analytics and Monitoring
-// router.get("/reports")
-// router.get("/messageMetrics")
+//Template Management
+router.get("/templates")
+router.get("/templates/:templateId")
+router.post("/editTemplate/:templateId")
+
+
+// Analytics and Monitoring
+router.get("/reports")
+router.get("/messageMetrics")
 
 
